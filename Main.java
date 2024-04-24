c1b597baaf0742e6bb0ef5f8d448554afd5b7033
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-24 16:32:44 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         PenjadwalanAcara penjadwalan = new PenjadwalanAcara();
 
-        KelasPBO kelasPBO = new KelasPBO("Kelas PBO", "Ruang 101", "Kamis", "09:00", "Pemrograman Berorientasi Objek");
+        KelasPBO kelasPBO = new KelasPBO("Kelas PBO", "Ruang B4B", "Kamis", "10:00", "Pemrograman Berorientasi Objek");
         penjadwalan.tambahAcara(kelasPBO);
 
-        RapatPengurus rapatPengurus = new RapatPengurus("Rapat Pengurus HMTI", "Ruang 202", "Senin", "14:00", "Pembahasan Kegiatan HMTI");
+        RapatPengurus rapatPengurus = new RapatPengurus("Rapat Pengurus HMTI", "Ruang B4C", "Senin", "14:00", "Pembahasan Kegiatan HMTI");
         penjadwalan.tambahAcara(rapatPengurus);
 
-        LatihanNgobar latihanNgobar = new LatihanNgobar("Latihan Ngobar", "Ruang 303", "Sabtu", "10:00", "Pemrograman Java");
+        LatihanNgobar latihanNgobar = new LatihanNgobar("Latihan Ngobar", "Ruang B4A", "Sabtu", "10:00", "Pemrograman Java");
         penjadwalan.tambahAcara(latihanNgobar);
 
         penjadwalan.tampilkanSemuaAcara();
